3121301d881bc9077d95bb586ad65d0d81ad27ad
Ray DeCampo <ray@decampo.org>
2021-09-26 18:03:32 -0400

    Add tag support
    Make a collection for the tags stealing code from
    https://github.com/philhawksworth/hawksworx.com/blob/master/src/site/_filters/getTagList.js.
    Then fix up tag.html accordingly.
    
    Also fix up the post.html template to handle tags and title (are we buying a
    car here?).

--- .eleventy.js
+++ .eleventy.js
@@ -6,8 +6,8 @@ module.exports = (eleventyConfig) => {
 
   eleventyConfig.addPlugin(pluginRss);
 
-  eleventyConfig.addCollection('posts', function(collection) {
-    return collection.getFilteredByGlob('src/_posts/*').reverse();
+  eleventyConfig.addCollection('posts', function(collections) {
+    return collections.getFilteredByGlob('src/_posts/*').reverse();
   });
 
   eleventyConfig.addCollection('featuredPosts', function(collections) {
@@ -25,7 +25,25 @@ module.exports = (eleventyConfig) => {
   eleventyConfig.setFrontMatterParsingOptions({
     excerpt: true,
     excerpt_separator: '<!-- excerpt -->'
-   });
+  });
+
+  // TAGS
+  eleventyConfig.addCollection('tags', function(collections) {
+    const tagSet = new Set();
+    collections.getFilteredByGlob('src/_posts/*').forEach(function(item) {
+      if( "tags" in item.data ) {
+        let tags = item.data.tags;
+        if( typeof tags === "string" ) {
+          tags = [tags];
+        }
+  
+        for (const tag of tags) {
+          tagSet.add(tag);
+        }
+      }
+    });
+    return [...tagSet].sort();
+  });
 
   return {
     dir: {
