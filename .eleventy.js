66dbf6bd2fcec064770b73d9d176d2fe49b8c2e7
Ray DeCampo <ray@decampo.org>
2021-09-26 20:09:51 -0400

    Fix favicon for eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = (eleventyConfig) => {
   // Copy the "assets" directory to the compiled "_site" folder.
   eleventyConfig.addPassthroughCopy('assets');
+  eleventyConfig.addPassthroughCopy({'assets/favicon': '/'});
 
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(syntaxHighlight, {
